134e16024e75c44bb171de6f25e0f7ad6118e0f6
toto101230 <tpienne51@gmail.com>
2023-11-30 17:38:54 +0100

    Ajout slider et Test collision

--- src/main/java/visualisation/VueDebug.java
+++ src/main/java/visualisation/VueDebug.java
@@ -1,8 +1,10 @@
 package visualisation;
 
+import controleur.Controleur;
+import etats.EtatExciteActive;
+import etats.EtatNormalActive;
 import etats.EtatParticule;
-import particules.Particule;
-import particules.ParticuleType;
+import particules.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -24,8 +26,8 @@ public class VueDebug implements Observer {
                     particules[i]--;
                 }
             }
-            if(particule.getEtat().getClass().getSimpleName().contains("Normal")) nbNormal--;
-            if(particule.getEtat().getClass().getSimpleName().contains("Excite")) nbExcite--;
+            if (particule.getEtat().getClass().getSimpleName().contains("Normal")) nbNormal--;
+            if (particule.getEtat().getClass().getSimpleName().contains("Excite")) nbExcite--;
         }
         majNbParticulesText();
     }
@@ -45,18 +47,18 @@ public class VueDebug implements Observer {
     }
 
     @Override
-    public void updateEtat(EtatParticule oldEtat, EtatParticule newEtat){
-        if(oldEtat.getClass().getSimpleName().contains("Normal")) nbNormal--;
-        if(oldEtat.getClass().getSimpleName().contains("Excite")) nbExcite--;
-        if(oldEtat.getClass().getSimpleName().contains("Jeune")) nbJeune--;
-        if(oldEtat.getClass().getSimpleName().contains("Active")) nbActive--;
-        if(oldEtat.getClass().getSimpleName().contains("FinDeVie")) nbFinDeVie--;
+    public void updateEtat(EtatParticule oldEtat, EtatParticule newEtat) {
+        if (oldEtat.getClass().getSimpleName().contains("Normal")) nbNormal--;
+        if (oldEtat.getClass().getSimpleName().contains("Excite")) nbExcite--;
+        if (oldEtat.getClass().getSimpleName().contains("Jeune")) nbJeune--;
+        if (oldEtat.getClass().getSimpleName().contains("Active")) nbActive--;
+        if (oldEtat.getClass().getSimpleName().contains("FinDeVie")) nbFinDeVie--;
 
-        if(newEtat.getClass().getSimpleName().contains("Normal")) nbNormal++;
-        if(newEtat.getClass().getSimpleName().contains("Excite")) nbExcite++;
-        if(newEtat.getClass().getSimpleName().contains("Jeune")) nbJeune++;
-        if(newEtat.getClass().getSimpleName().contains("Active")) nbActive++;
-        if(newEtat.getClass().getSimpleName().contains("FinDeVie")) nbFinDeVie++;
+        if (newEtat.getClass().getSimpleName().contains("Normal")) nbNormal++;
+        if (newEtat.getClass().getSimpleName().contains("Excite")) nbExcite++;
+        if (newEtat.getClass().getSimpleName().contains("Jeune")) nbJeune++;
+        if (newEtat.getClass().getSimpleName().contains("Active")) nbActive++;
+        if (newEtat.getClass().getSimpleName().contains("FinDeVie")) nbFinDeVie++;
         majNbParticulesText();
     }
 
@@ -83,7 +85,7 @@ public class VueDebug implements Observer {
         this.texte.setText(texte.toString());
     }
 
-    public Component getMenuDebug() {
+    public Component getMenuDebug(Controleur controleur) {
         JMenu mDebug = new JMenu("Debug");
         JMenuItem miDebug = new JMenuItem("Afficher les détails");
         miDebug.addActionListener(new ActionListener() {
@@ -94,6 +96,64 @@ public class VueDebug implements Observer {
             }
         });
         mDebug.add(miDebug);
+
+        JMenuItem miDebug2 = new JMenuItem("Test collision");
+        JOptionPane jop = new JOptionPane();
+        miDebug2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int type1 = jop.showOptionDialog(null, "Type de 1", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"A", "B", "C"}, null);
+//                int etat = jop.showOptionDialog(null, "Cycle de vie de 1", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Jeune", "Active", "FinDeVie"}, null);
+                int excite = jop.showOptionDialog(null, "Etat de 1", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Normal", "Excite"}, null);
+
+                int type2 = jop.showOptionDialog(null, "Type de 2", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"A", "B", "C"}, null);
+//                int etat2 = jop.showOptionDialog(null, "Cycle de vie de 2", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Jeune", "Active", "FinDeVie"}, null);
+                int excite2 = jop.showOptionDialog(null, "Etat de 2", "Test collision", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Normal", "Excite"}, null);
+
+                ParticuleType typeParticule1 = ParticuleType.values()[type1];
+                ParticuleType typeParticule2 = ParticuleType.values()[type2];
+                Particule p1 = null;
+                switch (typeParticule1) {
+                    case A:
+                        p1 = FabriqueParticuleA.getInstance().creationParticule(controleur.getchampParticules(), 100, 100, 0);
+                        break;
+                    case B:
+                        p1 = FabriqueParticuleB.getInstance().creationParticule(controleur.getchampParticules(), 100, 100, 0);
+                        break;
+//                    case C:
+//                        p1 = FabriqueParticuleC.getInstance().creationParticule(champ, 50, 50, 0);
+//                        break;
+                }
+                Particule p2 = null;
+                switch (typeParticule2) {
+                    case A:
+                        p2 = FabriqueParticuleA.getInstance().creationParticule(controleur.getchampParticules(), 800, 100, Math.PI);
+                        break;
+                    case B:
+                        p2 = FabriqueParticuleB.getInstance().creationParticule(controleur.getchampParticules(), 800, 100, Math.PI);
+                        break;
+//                    case C:
+//                        p2 = FabriqueParticuleC.getInstance().creationParticule(champ, 50, 50, 0);
+                    // break;
+                }
+                if (excite == 0) p1.setEtat(new EtatNormalActive(p1));
+                if (excite2 == 0) p2.setEtat(new EtatNormalActive(p2));
+
+                if (excite == 1) p1.setEtat(new EtatExciteActive(p1));
+                if (excite2 == 1) p2.setEtat(new EtatExciteActive(p2));
+
+
+                controleur.getchampParticules().addParticule(p1);
+                controleur.getchampParticules().addParticule(p2);
+            }
+        });
+        mDebug.add(miDebug2);
+
+        JSlider slider = new JSlider(10, 1000, 30);
+        slider.setMajorTickSpacing(10);
+        slider.addChangeListener(e -> controleur.getSim().setDelaiSimulation(slider.getValue()));
+        mDebug.add(slider);
+
         return mDebug;
     }
 
